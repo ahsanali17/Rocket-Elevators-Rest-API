2216a204808737910435bb32b053623c0e69aaac
Ahsan Ali Syed <ahsantime1@gmail.com>
2021-03-26 21:30:03 -0400

    restapi done

--- Controllers/InterventionsController.cs
+++ Controllers/InterventionsController.cs
@@ -20,18 +20,22 @@ namespace RestAPI.Controllers
             _context = context;
         }
 
+        //---------------------------------------Selects All Interventions------------------------------------------\\
+
         // GET: api/Interventions
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Intervention>>> GetIntervention()
+        public async Task<ActionResult<IEnumerable<Intervention>>> Getinterventions()
         {
-            return await _context.Intervention.ToListAsync();
+            return await _context.interventions.ToListAsync();
         }
+
+        //---------------------------------------Selects One Intervention---------------------------------------------\\
 
         // GET: api/Interventions/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Intervention>> GetIntervention(long id)
         {
-            var intervention = await _context.Intervention.FindAsync(id);
+            var intervention = await _context.interventions.FindAsync(id);
 
             if (intervention == null)
             {
@@ -41,9 +45,24 @@ namespace RestAPI.Controllers
             return intervention;
         }
 
+
+        //---------------------------------------GET INTERVENTION---------------------------------------------\\
+
+        // GET: api/Interventions/pending
+        [HttpGet("pending")]
+        public async Task<ActionResult<List<Intervention>>> GetPendingIntervention()
+        {
+            var allInterventions = await _context.interventions.Where(l => l.start_of_intervention == null).ToListAsync();
+            var newInterventions = allInterventions.Where(e => e.status == "Pending").ToList();
+            return newInterventions;
+        }
+
+
+        //---------------------------------------PUT #1 INTERVENTION---------------------------------------------\\
+
         // PUT: api/Interventions/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        [HttpPut("{id}/inprogress")]
         public async Task<IActionResult> PutIntervention(long id, Intervention intervention)
         {
             if (id != intervention.id)
@@ -51,7 +70,9 @@ namespace RestAPI.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(intervention).State = EntityState.Modified;
+            Intervention interventionFound = await _context.interventions.FindAsync(id);
+            interventionFound.status = intervention.status;
+            interventionFound.start_of_intervention = DateTime.Now;
 
             try
             {
@@ -72,12 +93,48 @@ namespace RestAPI.Controllers
             return NoContent();
         }
 
+        //---------------------------------------PUT #2 INTERVENTION---------------------------------------------\\
+
+        // PUT: api/Interventions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+      [HttpPut("{id}/completed")]
+        public async Task<IActionResult> PutIntervention2(long id, Intervention intervention)
+        {
+            if (id != intervention.id)
+            {
+                return BadRequest();
+            }
+
+            Intervention interventionFound = await _context.interventions.FindAsync(id);
+            interventionFound.status = intervention.status;
+            interventionFound.end_of_intervention = DateTime.Now;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InterventionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
         // POST: api/Interventions
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         // [HttpPost]
         // public async Task<ActionResult<Intervention>> PostIntervention(Intervention intervention)
         // {
-        //     _context.Intervention.Add(intervention);
+        //     _context.interventions.Add(intervention);
         //     await _context.SaveChangesAsync();
 
         //     return CreatedAtAction("GetIntervention", new { id = intervention.id }, intervention);
@@ -87,13 +144,13 @@ namespace RestAPI.Controllers
         // [HttpDelete("{id}")]
         // public async Task<IActionResult> DeleteIntervention(long id)
         // {
-        //     var intervention = await _context.Intervention.FindAsync(id);
+        //     var intervention = await _context.interventions.FindAsync(id);
         //     if (intervention == null)
         //     {
         //         return NotFound();
         //     }
 
-        //     _context.Intervention.Remove(intervention);
+        //     _context.interventions.Remove(intervention);
         //     await _context.SaveChangesAsync();
 
         //     return NoContent();
@@ -101,7 +158,8 @@ namespace RestAPI.Controllers
 
         private bool InterventionExists(long id)
         {
-            return _context.Intervention.Any(e => e.id == id);
+            return _context.interventions.Any(e => e.id == id);
         }
     }
+    
 }
--- Models/Intervention.cs
+++ Models/Intervention.cs
@@ -2,4 +2,27 @@ using System;
 public class Intervention
 {
     public long id { get; set; }
+    public long author { get; set; }
+    public long customer_id { get; set; }
+    public long building_id { get; set; }
+    public long? battery_id { get; set; }
+    public long? column_id { get; set; }
+    public long? elevator_id { get; set; }
+    public long? employee_id { get; set; }
+
+    public DateTime? start_of_intervention { get; set; }
+
+    public DateTime? end_of_intervention { get; set; }
+
+    public string result { get; set; }
+    public string report { get; set; }
+    public string status { get; set; }
+
+    // public virtual Employees Author { get; set; }
+    // public virtual Batteries Battery { get; set; }
+    // public virtual Buildings Building { get; set; }
+    // public virtual Columns Column { get; set; }
+    // public virtual Customers Customer { get; set; }
+    // public virtual Elevators Elevator { get; set; }
+    // public virtual Employees Employee { get; set; }
 }
